9f1cd934e95e59f39f5c693261e4215a5625c58a
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-07-18 20:08:17 -0300

    Add result

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,11 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Timer = () => {
   const dataAtual = new Date();
-  const hora = dataAtual.getHours();
-  const minutos = dataAtual.getMinutes();
+  const hora = (dataAtual.getHours() < 10 ? '0' : '') + dataAtual.getHours()
+  const minutos = (dataAtual.getMinutes() < 10 ? '0' : '') + dataAtual.getMinutes();
   const [initialHour, setInicialHour] = useState('00:00');
   const [finalHour, setFinalHour] = useState('00:00');
+  const [count, setCount] = useState(0)
+  const [conta, setConta] = useState(0)
+
+  useEffect(() => {
+      setTimeout(() => setCount(count + 1), 1000);
+      if (initialHour != '00:00') {
+        setConta(count)
+      }
+      if (finalHour != '00:00') {
+        setConta(conta)
+      }
+
+  }, [count]);
+
+
 
   return (
     <div className='timeLine'>
@@ -17,6 +32,9 @@ const Timer = () => {
       <div>
         <p>{finalHour}</p>
         <button onClick={() => setFinalHour(hora + ":" + minutos)}>stop</button>
+      </div>
+      <div>
+        <p>Tempo corrido = {conta}</p>
       </div>
     </div>
   )
